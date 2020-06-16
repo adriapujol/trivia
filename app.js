614713bab2e1bb8fa63b1aff423e6a3daaf700ac
Adria Pujol <adriapujolaparicio@gmail.com>
2020-06-16 17:26:59 +0200

    working but shitty responsive

--- app.js
+++ app.js
@@ -22,15 +22,23 @@ const dataController = (()=> {
 
     //Create questions
 
-    const question1 = new Question(1, 'What is 2 + 2 ?', ['1', '2', '3', '4'], '4');
-    const question2 = new Question(2, 'How many hands humas have?', ['11', '2', '25', '4'], '2');
-    const question3 = new Question(3, 'What color is the sky?', ['Red', 'Yellow', 'Blue', 'Orange'], 'Blue');
+    const question1 = new Question(1, 'For how long can a snail sleep?', ['12 hours', '1 week', '6 months', '3 years'], '3 years');
+    const question2 = new Question(2, 'How many penises does a shark have?', ['2', '1', '69', 'What is a penis?'], '2');
+    const question3 = new Question(3, "Where is the heart of a shrimp located?", ['Chest', 'Head', 'Body', 'Penis'], 'Head');
+    const question4 = new Question(4, "How many noses does a slug have?", ["1", "4", "3", "0"], "4");
+    const question5 = new Question(5, 'Which one is bigger?', ["Ostrich's eye", "Ostrich's brain"], "Ostrich's eye");
+    const question6 = new Question(6, "What is a rhino's horn made of?", ['Ivori', 'Hair', 'You dreams and hopes', 'Definetly a penis'], 'Hair');
+    const question7 = new Question(7, 'Can a kangaroo fart?', ['Yes', 'No', 'Only after eating Taco Bell'], 'No');
+    const question8 = new Question(8, "How many vaginas does female Koala Bears have?", ["1", "2", "0", "4"], "2");
+    const question9 = new Question(9, 'Are dolphins the worst?', ['Yes! F*ck those rapist!', 'Not at all', 'Flipper was good so they are cool', 'Flipper sucks too'], 'Yes! F*ck these rapist!');
+    const question10 = new Question(10, "What is the take away of this?", ["No idea, I have the brain of an ostrich", "F*ck the dolphins", "Rhino's horns are lame", "Sharks and female Koalas are a perfect match"], "Sharks and female Koalas are a perfect match");
+
     
     //Game questions + score;
 
     const game = {
         score: 0,
-        questions: [question1, question2, question3],
+        questions: [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10],
     }
 
     return {
@@ -97,7 +105,7 @@ const UIController = (()=> {
         //  },
 
         setScore: (score, game) => {
-            score.innerHTML = `${game.score}/${game.questions.length}`
+            score.innerHTML = `Score: ${game.score}/${game.questions.length}`;
 
          },
 
@@ -147,6 +155,7 @@ const controller = ((dataCtrl, UIctrl)=> {
 
     //
     const renderGame = () => {
+        resetGame();
         btnStart.classList.add(DOM.hide);
         gameBox.classList.remove(DOM.hide);
         UIctrl.setScore(score, game);
@@ -187,19 +196,35 @@ const controller = ((dataCtrl, UIctrl)=> {
         }
     }
 
-    const resetQuestions = () => {
+    const resetGame = () => {
+        answersCount = 0;
+        game.score = 0;
+        final.classList.add(DOM.hide);
+        let span = document.getElementById('before-score');
+        if (span !== null) {
+            span.remove();
+        }
         for (item in game.questions) {
             game.questions[item].answered = false;
         }
     }
 
     const endGame = () => {
-        final.classList.remove('hide');
+        final.classList.remove(DOM.hide);
         let span = document.createElement('span');
-        span.innerHTML = 'Your Score is: ';
+        const bestScore = game.questions.length;
+
+        if(game.score < bestScore/2) {
+            span.innerHTML = "That wasn't that impressive";
+        } else if ( game.score === bestScore) {
+            span.innerHTML = "Nailed it! High five!";
+        } else {
+            span.innerHTML = "Not bad at all";
+        }
         span.className = 'before-score';
+        span.id = 'before-score';
         score.parentNode.insertBefore(span, score);
-        // btnStart.classList.remove('hide');
+        btnStart.classList.remove(DOM.hide);
     }
 
     const nextButton = () => {
